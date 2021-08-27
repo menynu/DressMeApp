08bc86b4a3dbf63ebcda0efc51e756e18bf8e7b3
Meny <meny205@gmail.com>
2021-08-27 12:23:12 +0300

    Initializing commit

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Dress Me App</Text>
+    </View>
+  );
+};
+export default App;
