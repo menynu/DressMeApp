c352e4bd25a3f6ad7c8ccb0f5a61f1c573152765
Meny <meny205@gmail.com>
2021-08-29 15:55:20 +0300

    added styled Button, mobx persist store & observer

--- src/screens/Item.js
+++ src/screens/Item.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Text, Button} from 'react-native';
+import {observer, inject} from 'mobx-react';
+import DressStore from '../stores/DressStore';
 
-const Item = () => {
-  return (
-    <View style={styles.center}>
-      <Text>This is the Item selection screen</Text>
-    </View>
-  );
-};
+const Item = inject('DressStore')(
+  observer(({Id, Brand, Colors, Sizes, Name, route, navigation}) => {
+    const [itemList, setItemList] = useState(
+      JSON.stringify(DressStore.itemStore),
+    );
+
+    return (
+      <View style={styles.center}>
+        <Text>
+          This is the Item selection screen this is {route.params.name}
+          {console.log('new: ', itemList)}
+          {/* {console.log(`this is test: ${DressStore.itemStore}`)} */}
+        </Text>
+        <Button title="Finish Button" onPress={() => navigation.goBack()} />
+      </View>
+    );
+  }),
+);
 
 const styles = StyleSheet.create({
   center: {
