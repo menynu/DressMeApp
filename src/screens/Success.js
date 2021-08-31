29580cca91608b684146ad68c023e91add502dbd
Meny <meny205@gmail.com>
2021-08-31 10:13:52 +0300

    added table of ordered list in success screen

--- src/screens/Success.js
+++ src/screens/Success.js
@@ -16,21 +16,40 @@ const Success = inject('DressStore')(
 
     return (
       <View style={styles.center}>
-        <Text>Congratulation! you have completed a dressing set!</Text>
+        <Text style={{color: 'green', fontSize: 22, marginBottom: 20}}>
+          Congratulation! you have completed a dressing set!
+        </Text>
         <Image
           source={{
             uri: `https://picsum.photos/200?${Date.now()}`, // fix the image cache problem
           }}
           style={{width: 200, height: 200}}
         />
-        <Text>
+        <Text style={{fontSize: 15, marginBottom: 10}}>
           It took you {Math.round((Date.now() - DressStore.time) / 1000)}{' '}
           seconds to choose!
         </Text>
-        <Text>
+        <Text style={{fontSize: 15}}>
           Sum of the shoes and pants sizes :
           {dress[0].selectedSize + dress[1].selectedSize}
         </Text>
+        <View style={{flexDirection: 'row'}}>
+          {dress.map((item, index) => {
+            const {type, name, brand, selectedSize, selectedColor} = item;
+            return (
+              <View style={{flexDirection: 'column', margin: 5}}>
+                <Text
+                  style={{fontSize: 22, color: 'orange', fontWeight: 'bold'}}>
+                  {type}
+                </Text>
+                <Text>Name : {name}</Text>
+                <Text>Brand : {brand} </Text>
+                <Text>Size : {selectedSize}</Text>
+                <Text>Color : {selectedColor}</Text>
+              </View>
+            );
+          })}
+        </View>
         <Button
           title="Choose another set"
           onPress={() => {
